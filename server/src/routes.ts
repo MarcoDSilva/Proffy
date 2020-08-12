88167af676c1267367bdfdebd2ad35932f99fc81
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-08-12 17:35:44 +0100

    cors bug fix

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,11 +5,11 @@ import ConnectionController from "./controllers/ConnectionsController";
 const routes = express.Router();
 const classesController = new ClassesController();
 const connectionsController = new ConnectionController();
-
+ 
 routes.get("/classes", classesController.index);
 routes.post("/classes", classesController.create);
 
-routes.get("/controllers", connectionsController.index);
-routes.post("/controllers", connectionsController.create);
+routes.get("/connections", connectionsController.index);
+routes.post("/connections", connectionsController.create);
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,13 @@
 import express from "express";
+import cors from 'cors'
 import routes from "./routes";
 
 const app = express();
 
+app.use(cors());
 //express needs to be alerted to use json
 app.use(express.json());
-app.use(routes)
-
+app.use(routes);
 
 //listens the port from the adress
 app.listen(3337);
